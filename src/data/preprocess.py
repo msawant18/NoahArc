8ae3645f99668e9c2959faa26e5894d5da39f341
msawant18 <111610357+msawant18@users.noreply.github.com>
2025-09-17 20:05:52 -0700

    Update preprocess.py

--- src/data/preprocess.py
+++ src/data/preprocess.py
@@ -1,1 +1,6 @@
-
+"""
+Preprocessing of SAR tiles.
+"""
+def preprocess_tile(tile_path: str, out_path: str):
+    # TODO: Add speckle filtering, normalization, tiling
+    print(f"[INFO] Preprocessing {tile_path} -> {out_path}")
