2aa45fa252d990ff7f86adb1ea13f70ca7e8fa01
msawant18 <111610357+msawant18@users.noreply.github.com>
2025-09-17 20:06:48 -0700

    Update unet.py

--- src/models/unet.py
+++ src/models/unet.py
@@ -1,1 +1,14 @@
+"""
+UNet model for segmentation.
+"""
+import torch
+import torch.nn as nn
 
+class UNet(nn.Module):
+    def __init__(self, in_channels=1, out_channels=1):
+        super().__init__()
+        # TODO: implement full UNet
+        self.dummy = nn.Conv2d(in_channels, out_channels, 1)
+
+    def forward(self, x):
+        return torch.sigmoid(self.dummy(x))
